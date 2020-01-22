20861e591c60b9c0733706a2a1b0d2d525ae4da0
Prafful Daga <prraful@gmail.com>
2020-01-22 16:30:36 +0530

    spring mvc

--- 007-SpringMVC-HelloWorld/WebContent/WEB-INF/myviews/addpage.jsp
+++ 007-SpringMVC-HelloWorld/WebContent/WEB-INF/myviews/addpage.jsp
@@ -4,14 +4,10 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Hello</title>
+<title>Add Friend</title>
 </head>
 <body>
-	<h1>Hello Spring MVC</h1>
-	<h4>13</h4>
-	<h3>Name:</h3><h2> ${name}</h2>
-	<h3>Location:</h3> <h2>${location}</h2>
-	<hr>
-	Add friend <a href="add.html">Add</a>
+<h3>Name:</h3><h2> ${company}</h2>
+<h3>Location:</h3> <h2>${branch}</h2>
 </body>
 </html>
--- 007-SpringMVC-HelloWorld/src/com/springmvc/hello/controller/FirstSpringMVCHelloController.java
+++ 007-SpringMVC-HelloWorld/src/com/springmvc/hello/controller/FirstSpringMVCHelloController.java
@@ -2,21 +2,46 @@ package com.springmvc.hello.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FirstSpringMVCHelloController {
 
-	@RequestMapping("/")
-	public String welcome() {
+	/**
+	 *   /home will be interpreted as home.spring
+	 *   because url-pattern is set to *.spring
+	 */ 
+	@RequestMapping("/home")
+	public ModelAndView welcome() {
 		/**
 		 * it will look for 
 		 * prefix + welcome + suffix
 		 * i.e. 
-		 * /WEB-INF/myviews/welcome.jsp
+		 * /WEB-INF/myviews/hello.jsp
 		 */
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", "Prafful Daga");
+		mv.addObject("location", "Chennai");
+		mv.setViewName("hello");
+		return mv;
 		
 		//return "welcome";
-		return "hello";
+		
+	}
+	
+	@RequestMapping("/add")
+	public ModelAndView addFriendPage() {
+		/**
+		 * it will look for 
+		 * prefix + welcome + suffix
+		 * i.e. 
+		 * /WEB-INF/myviews/addpage.jsp
+		 */
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("company", "CTS");
+		mv.addObject("branch", "Kochi");
+		mv.setViewName("addpage");
+		return mv;
 	}
 	
 }
